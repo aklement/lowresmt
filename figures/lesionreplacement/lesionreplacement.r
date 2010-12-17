b6648b227fb796219a6d72f3ebd17013292cf4be
Alexandre Klementiev <aklement+github@gmail.com>
2010-12-16 20:58:58 -0500

    Added to the experiments section, merged the main graph.

---
+++ figures/lesionreplacement/lesionreplacement.r
@@ -0,0 +1,25 @@
+c <- read.table(file="lesionreplacement.txt", sep = "\t", head=TRUE)
+
+x <- c$BLEU
+names(x) <- c$Title
+
+colorz <- c("green", "blue", "orange", "red", "orange", "blue", "blue", "blue", "green", "green")
+fillz <- c(25, 25, 25, 25, 25, 25, 25, 25, 25, 25)
+anglez <- c(135, 135, 135, 135, 45, 45, 45, 45, 45, 45)
+
+saxis <- 1.3
+slabels <- 1.3
+snames <- 1.3
+
+pdf(file="lesionreplacement.pdf", height=6, width=14, onefile=TRUE, family='Helvetica', pointsize=12)
+par(mar=c(4.5,4.5,3.5,0.5), new=FALSE, cex.lab=slabels)
+
+bp <-barplot(x, density=fillz, col=colorz, angle=anglez, ylab="BLEU", cex.lab=slabels, cex.axis=saxis, cex.names=snames, ylim=c(0, 25), legend=F)
+grid(nx = 0, ny = NULL, col = "black", lty = "dotted", lwd = 1, equilogs = FALSE)
+#legend("topright", as.character(c$Title), cex=slabels, density=fillz, fill=colorz, bty="y", bg="white")
+box()
+
+height <- rbind(x)
+text(bp, height, labels=format(height, 4), pos=3, cex=snames)
+
+dev.off()
