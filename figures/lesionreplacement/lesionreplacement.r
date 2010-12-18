7b9167cd6ebdf09429f26a50c5c684aa4fcda655
Alexandre Klementiev <aklement+github@gmail.com>
2010-12-17 21:07:26 -0500

    Reformatted Figure 7.

--- figures/lesionreplacement/lesionreplacement.r
+++ figures/lesionreplacement/lesionreplacement.r
@@ -1,20 +1,21 @@
 c <- read.table(file="lesionreplacement.txt", sep = "\t", head=TRUE)
 
 x <- c$BLEU
-names(x) <- c$Title
+#names(x) <- c$Title
 
 colorz <- c("green", "blue", "orange", "red", "orange", "blue", "blue", "blue", "green", "green")
 fillz <- c(25, 25, 25, 25, 25, 25, 25, 25, 25, 25)
 anglez <- c(135, 135, 135, 135, 45, 45, 45, 45, 45, 45)
+spacez <- c(0.4, 0.4, 0.4, 0.4, 1.2, 0.4, 0.4, 0.4, 0.4, 1.2)
 
 saxis <- 1.3
 slabels <- 1.3
 snames <- 1.3
 
-pdf(file="lesionreplacement.pdf", height=6, width=14, onefile=TRUE, family='Helvetica', pointsize=12)
-par(mar=c(4.5,4.5,3.5,0.5), new=FALSE, cex.lab=slabels)
+pdf(file="lesionreplacement.pdf", height=4, width=11, onefile=TRUE, family='Helvetica', pointsize=12)
+par(mar=c(1.0,4.8,1.0,0.5), new=FALSE, cex.lab=slabels)
 
-bp <-barplot(x, density=fillz, col=colorz, angle=anglez, ylab="BLEU", cex.lab=slabels, cex.axis=saxis, cex.names=snames, ylim=c(0, 25), legend=F)
+bp <-barplot(x, density=fillz, col=colorz, angle=anglez, space=spacez, ylab="BLEU", cex.lab=slabels, cex.axis=saxis, cex.names=snames, ylim=c(0, 25), legend=F)
 grid(nx = 0, ny = NULL, col = "black", lty = "dotted", lwd = 1, equilogs = FALSE)
 #legend("topright", as.character(c$Title), cex=slabels, density=fillz, fill=colorz, bty="y", bg="white")
 box()
