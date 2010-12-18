9d2299e1a14cbb593e8ef61b2774d6f8e835ffd6
Alexandre Klementiev <aklement+github@gmail.com>
2010-12-17 22:19:54 -0500

    Removed middle histogram pair from Fig. 4.

--- figures/temporal/temporal.r
+++ figures/temporal/temporal.r
@@ -70,10 +70,12 @@ for (i in c(from:to)) {
 
 colors <- c("red", "blue")
 
-saxis <- 1.3
-slabels <- 1.3
+saxis <- 1.2
+slabels <- 1
 
-pdf(file="temporal.pdf", height=6, width=7, onefile=TRUE, family='Helvetica', pointsize=14)
+pdf(file="temporal.pdf", height=4, width=7, onefile=TRUE, family='Helvetica', pointsize=12)
+#pdf(file="temporal.pdf", height=6, width=7, onefile=TRUE, family='Helvetica', pointsize=14)
+
 
 k = c(1,1,1,1,1,1,1)#1,1,1)#,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)            # k is the vector of weights
 k = k/sum(k)
@@ -85,23 +87,24 @@ d[,4] = - filter(d[,4], sides=2, k) / sum(d[,4])
 
 r <- range(te$V1, ts$V2)
 
-par(mar=c(0.25,2,0.25,0.5), mfrow=c(3,1), oma=c(2,0,0.25,0))
+par(mar=c(0.25,2,0.25,0.5), mfrow=c(2,1), oma=c(2,0,0.25,0))
+#par(mar=c(0.25,2,0.25,0.5), mfrow=c(3,1), oma=c(2,0,0.25,0))
 
 plot(d[,1], ylim = c(-0.015,0.015),  type="h", lwd=1.5, col=colors[1], xaxt='n', yaxt='n', ann = FALSE)
 lines(d[,2], type="h", col=colors[2], lty=1, lwd=1.5)
 
 legend("topright", cex=slabels, c("terrorist (en)","terrorista (es)"), col=colors, lty=c(1,1), lwd=1.5, bty="n");
 
-mtext("Occurrences", side=2,line=0.5,font=1)
+mtext("Occurrences", side=2,line=0.5,font=1, cex=saxis)
 
-par(mar=c(0.25,2,0.25,0.5))
+#par(mar=c(0.25,2,0.25,0.5))
 
-plot(d[,1], ylim = c(-0.015,0.015),  type="h", lwd=1.5, col=colors[1], xaxt='n', yaxt='n', ann = FALSE)
-lines(d[,3], type="h", col=colors[2], lty=1, lwd=1.5)
+#plot(d[,1], ylim = c(-0.015,0.015),  type="h", lwd=1.5, col=colors[1], xaxt='n', yaxt='n', ann = FALSE)
+#lines(d[,3], type="h", col=colors[2], lty=1, lwd=1.5)
 
-legend("topright", cex=slabels, c("terrorist (en)","ataques (es)"), col=colors, lty=c(1,1), lwd=1.5, bty="n");
+#legend("topright", cex=slabels, c("terrorist (en)","ataques (es)"), col=colors, lty=c(1,1), lwd=1.5, bty="n");
 
-mtext("Occurrences", side=2,line=0.5,font=1)
+#mtext("Occurrences", side=2,line=0.5,font=1)
 #mtext("Time", side=1,line=0.5,font=1)
 
 par(mar=c(0.25,2,0.25,0.5))
@@ -111,8 +114,8 @@ lines(d[,4], type="h", col=colors[2], lty=1, lwd=1.5)
 
 legend("topright", cex=slabels, c("terrorist (en)","riqueza (es)"), col=colors, lty=c(1,1), lwd=1.5, bty="n");
 
-mtext("Occurrences", side=2,line=0.5,font=1)
-mtext("Time", side=1,line=0.5,font=1)
+mtext("Occurrences", side=2,line=0.5,font=1, cex=saxis)
+mtext("Time", side=1,line=0.5,font=1, cex=saxis)
 
 
 dev.off()
